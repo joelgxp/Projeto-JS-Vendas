ced1c0faece5a7d1edfcd4a7fa9adb49583be56c
Joel Souza <joelvieirasouza@gmail.com>
2021-09-06 12:44:38 -0300

    Project created

---
+++ backend/src/test/java/com/devj/jsvendas/JsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devj.jsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
