3e3391c7cba5659b8340144391893d8ad7b463ae
Joel Souza <joelvieirasouza@gmail.com>
2021-09-06 22:26:23 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
 import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">JS Vendas</h1>
+        
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
