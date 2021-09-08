089ca05a8f68135a4126376f97917960f80f0380
Joel Souza <joelvieirasouza@gmail.com>
2021-09-08 14:01:08 -0300

    Layers

---
+++ backend/src/main/java/com/devj/jsvendas/JsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devj.jsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/devj/jsvendas/services/SellerService.java
@@ -0,0 +1,24 @@
+package com.devj.jsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devj.jsvendas.dto.SellerDTO;
+import com.devj.jsvendas.entities.Seller;
+import com.devj.jsvendas.repositories.SellerRepositories;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepositories repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+}
